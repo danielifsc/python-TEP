072acd59fe1abcde50225ba25f93450eb4f4812f
dan <daniel.ma09@aluno.ifsc.edu.br>
2026-02-11 18:15:08 -0300

    att django 3

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import index
+from .views import contatos
 
 
 urlpatterns = [
-    path('', index),
+    path('', index, name="urlindex"),
+    path('contatos', contatos, name="urlcontatos"),
 ]
--- aplicacao/views.py
+++ aplicacao/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
+    context = {'curso': 'Desenvolvimento de Sistemas'}
+    return render(request, 'index.html', context)
 
-    return render(request, 'index.html')
+def contatos(request):
+    context = {'telefone' : '2050-2050', 'email' : 'carlinhos@gmail.com'}
+    return render(request, 'contatos.html', context)
 # Create your views here.
