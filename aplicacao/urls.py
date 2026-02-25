6757206d56c6a64b6e934d1fd578e1c18a57d7c1
dbernardos <daniel.ma09@aluno.ifsc.edu.br>
2026-02-25 18:31:43 -0300

    django-att

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from .views import index
 from .views import contatos
-
+from .views import produto
+from .views import cadastrarProduto
+from .views import salvarProduto
+from .views import editarProduto
+from .views import excluirProduto
 
 urlpatterns = [
     path('', index, name="urlindex"),
     path('contatos', contatos, name="urlcontatos"),
+    path('produto', produto, name="urlproduto"),
+    path('cadastrarProduto', cadastrarProduto, name="urlcadastrarProduto"),
+    path('salvarProduto', salvarProduto, name="urlsalvarProduto"),
+    path('editarProduto/<int:id>', editarProduto, name="urleditarProduto"),
+    path('excluirProduto/<int:id>', excluirProduto, name="urlexcluirProduto")
 ]
--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Produto
 
 def index(request):
     context = {'curso': 'Desenvolvimento de Sistemas'}
@@ -8,4 +9,69 @@ def index(request):
 def contatos(request):
     context = {'telefone' : '2050-2050', 'email' : 'carlinhos@gmail.com'}
     return render(request, 'contatos.html', context)
+def produto(request):
+    produtos = Produto.objects.all()
+    context = {'produtos': produtos}
+    return render(request, "produto.html", context)
 # Create your views here.
+
+def cadastrarProduto(request):
+    return render(request, "cadastrarProduto.html")
+
+def salvarProduto(request):
+    thisnome = request.POST.get('txtNome')  
+    thispreco = request.POST.get('txtPreco').replace(',','.')   
+    thisqtde = request.POST.get('txtQtde') 
+    thisdescricao = request.POST.get('txtDescricao')
+    thisdata = request.POST.get('txtData')
+
+    produto.nome = thisnome
+    produto.preco = float(thispreco)
+    produto.qtde = thisqtde
+    produto.descricao = thisdescricao
+    produto.data = thisdata
+
+    produto.save()
+    return redirect('urlproduto')
+
+def editarProduto(request, id):
+    if request.method == "GET":
+        context = {'p': produto}
+        return render(request, "editarProduto.html", context)
+    else:
+
+      thisnome = request.POST.get('txtNome')  
+      thispreco = request.POST.get('txtPreco').replace(',','.')  
+      thisqtde = request.POST.get('txtQtde') 
+      thisdescricao = request.POST.get('txtDescricao')
+      thisdata = request.POST.get('txtData')
+
+      produto.nome = thisnome
+      produto.preco = float(thispreco)
+      produto.qtde = thisqtde
+      produto.descricao = thisdescricao
+      produto.data = thisdata
+
+    produto.save()
+    return redirect('urlproduto')
+
+def excluirProduto(request, id):
+    if request.method == "GET":
+        context = {'p': produto}
+        return render(request, "excluirProduto.html", context)
+    else:
+
+      thisnome = request.POST.get('txtNome')  
+      thispreco = request.POST.get('txtPreco').replace(',','.')   
+      thisqtde = request.POST.get('txtQtde') 
+      thisdescricao = request.POST.get('txtDescricao')
+      thisdata = request.POST.get('txtData')
+
+      produto.nome = thisnome
+      produto.preco = float(thispreco)
+      produto.qtde = thisqtde
+      produto.descricao = thisdescricao
+      produto.data = thisdata
+ 
+    produto.save()
+    return redirect('urlproduto')
